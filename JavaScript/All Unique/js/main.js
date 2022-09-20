5d58f07319363e4c3a8c54e9e7e1fa0f9b4ecb75
Jesus Porras <102770370+Polymatio@users.noreply.github.com>
2022-09-20 13:09:36 -0700

    updating files

--- JavaScript/All Unique/js/main.js
+++ JavaScript/All Unique/js/main.js
@@ -1,4 +1,7 @@
 // Create a function that contains only unique characters.
+// Using the set.size object will allow to store unique values
+// The length property will let help count the number of unique values in the string
+// It will return a number of unique values in the string
 
 function hasUniqueChars(str) {
     return new Set(str).size == str.length
--- JavaScript/Blood Moon/js/main.js
+++ JavaScript/Blood Moon/js/main.js
@@ -1,4 +1,6 @@
-//create a function to find the Radius of AGFHA
+// Create a function to find the Radius of AGFHA
+// Return the radius for the semi circle of AGFHA 
+
 function bloodMoon(r) {
     return r*r/4;
 }
