f1234637205bf6feeca959ec6c62b83ad49b6289
Jesus Porras <102770370+Polymatio@users.noreply.github.com>
2022-11-21 15:39:35 -0800

    update file

--- JavaScript/Remove Consecutive Duplicate Words/js/main.js
+++ JavaScript/Remove Consecutive Duplicate Words/js/main.js
@@ -3,6 +3,6 @@
 // The filter method is used to find if their is no repeating word before it
 // Join method will connect the word back to after the filter case is finish and return
 
-function removeDuplicateWords(str) {
-    return str.split(" ").filter((w, i, a) => w != a[i - 1]).join(" ")
+function removeConsecutiveDuplicates(str) {
+    return str.split(" ").filter((w, i, a) => w != a[i - 1]).join(" ");
 }
